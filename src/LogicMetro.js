50d8c8209369f9c09f3c7ba444612565473c4ef5
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-23 19:46:19 +0100

    Refactoring

--- src/LogicMetro.js
+++ src/LogicMetro.js
@@ -1,28 +1,24 @@
-//State global du metronome
+// State global du metronome
 let state = {
-    //Numero de la mesure dans la partition
+    // Numero de la mesure dans le segment
     activeMesure: 1,
+    // Numero de la mesure dans la partition
     globalMesure:1,
-    ////index du segment
+    // index du segment
     indexActiveSegment: 0, 
-    //Numero du temps dans la mesure 
+    // Numero du temps dans la mesure 
     activeBeat: 1,
     playing: false,
     finished: false,
-  }
+}
 
 let globalResolve 
-
 
 const bips = {
     [Symbol.asyncIterator]() {
         return {
             count: 0,
             next() {
-                // this.count++
-                // if(this.count === 1){
-                //     return promise
-                // }
                 return new Promise((resolve, reject) => {
                     globalResolve = resolve
                 })  
@@ -31,7 +27,7 @@ const bips = {
     }
 }
 
-  //Charge le state depuis l'application
+// Charge le state depuis l'application
 export const init = (initialState) => {
     state.nom = initialState.nom
     state.segments = initialState.segments
@@ -42,97 +38,94 @@ const updateStatePosition = (state) => {
     let  indexActiveSegment = state.indexActiveSegment
     let {nbMesure, base} = state.segments[indexActiveSegment]
 
-    //Met a jour les infos sur la position du metronome dans la partition
-    let newState = updateBeat(state, base, nbMesure, indexActiveSegment)
-
-    return newState
+    // Met a jour les infos sur la position du metronome dans la partition
+    return updateBeat(state, base, nbMesure, indexActiveSegment)
 }
 
-//Update le state avec le nouveau numero de mesure
+// Update le state avec le nouveau numero de mesure
 const updateMesure = (state, nbMesure, indexActiveSegment) => {
     let newState = {...state}
-    //Check si c'est la derniere mesure du segment
+    // Check si c'est la derniere mesure du segment
     if (state.activeMesure + 1 > nbMesure) {
-        //Si oui, reset a la premiere mesure
+        // Si oui, reset a la premiere mesure
         newState.activeMesure = 1
-        //Check si c'est le dernier segment de la partition
+        // Check si c'est le dernier segment de la partition
         if (indexActiveSegment == state.segments.length - 1) {
-            //Si oui, renvoie que c'est finit
+            // Si oui, renvoie que c'est finit
             state.globalMesure = 1
             return {...newState, finished: true}
         } else {
-            //Passe au segment suivant dans la partition
+            // Passe au segment suivant dans la partition
             return {...newState, indexActiveSegment: indexActiveSegment + 1}
         }
-    //Sinon, le segment n'est pas finit        
+    // Sinon, le segment n'est pas finit        
     } else {
-        //Passe a la mesure suivante
+        // Passe a la mesure suivante
         newState.activeMesure = state.activeMesure + 1
         return newState
     }
 }
 
-//Gere la position dans la mesure (numero du temps)
+// Gere la position dans la mesure (numero du temps)
 const updateBeat = (state, base, nbMesure, indexActiveSegment) => { 
     let newState = {...state}
-    //Si on est a la fin de la mesure
+    // Si on est a la fin de la mesure
     if (state.activeBeat + 1 > base) {
-        //Reset au premier temps        
+        // Reset au premier temps        
         newState.activeBeat = 1
         newState.globalMesure = newState.globalMesure + 1
-        //Declenche update de la mesure
+        // Declenche update de la mesure
         newState = updateMesure(newState, nbMesure, indexActiveSegment)
-    //Sinon on passe au temps suivant
+    // Sinon on passe au temps suivant
     }else{
         newState.activeBeat = newState.activeBeat + 1
     }
     return newState
 }
 
-//
 let interval
+
 export const play = () => {
-    //Le metronome est en train de jouer
+    // Le metronome est en train de jouer
     state = { ...state, playing: true }
-    console.log('play')
-    //Si la partition est finit met fin a la recursion et clear l'interval
+    // Si la partition est finit met fin a la recursion et clear l'interval
     if(state.finished) {
         clearInterval(interval)
-        //MAJ du state
+        // MAJ du state
         state = { ...state, playing: false }
         return 
     }
-    //Extrait le tempo depuis le segment en cours
+    // Extrait le tempo depuis le segment en cours
     let { tempo } = state.segments[state.indexActiveSegment]
-    //Calcule le tempo en MS
+    // Calcule le tempo en MS
     let intervalMS = 60000 / tempo
 
-    //Si c'est le premier temps de la premiere mesure du segment
+    // Si c'est le premier temps de la premiere mesure du segment
     if(state.activeBeat == 1){
         globalResolve({ value: {activeBeat: state.activeBeat, globalMesure: state.globalMesure}, done: false })
         state = updateStatePosition(state)
     }
     
     interval = setInterval(() => {
-        //Si la partition est finit
+        // Si la partition est finit
         if(state.finished) {
-            //Nettoie l'interval
+            // Nettoie l'interval
             clearInterval(interval)
-            //Reset du state au default
+            // Reset du state au default
             state = { ...state, playing: false , globalMesure: 1, indexActiveSegment: 0, activeBeat: 1, finished:false }
             globalResolve({ done: true })   
             return 
         }
         globalResolve({ value: {activeBeat: state.activeBeat, globalMesure: state.globalMesure}, done: false })
-        //Save le dernier segment en cours
+        // Save le dernier segment en cours
         const lastSegment = state.indexActiveSegment
-        //MAJ de la position dans la partition
+        // MAJ de la position dans la partition
         state = updateStatePosition(state)
-        //Check si le segment est finit
+        // Check si le segment est finit
         if (lastSegment != state.indexActiveSegment) {
-            //Si oui nettoie l'interval
+            // Si oui nettoie l'interval
             clearInterval(interval)
-            //Appel le prochain segment
+            // Appel le prochain segment
            setTimeout(()=>{  play()}, intervalMS)
         }
 
